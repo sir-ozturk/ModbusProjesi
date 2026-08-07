5814dc86d61c2a4fff42c6d7725d8ead7d009812
sir-ozturk <usozturk11@gmail.com>
2026-08-08 00:58:39 +0300

    uq dosyası silindi

--- UQ/UQ_Kullanicilar_KullaniciAdi.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Kullanicilar
-ADD CONSTRAINT UQ_Kullanicilar_KullaniciAdi
-UNIQUE (kullanici_adi);
--- UQ/UQ_Kullanicilar_Mail.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Kullanicilar
-ADD CONSTRAINT UQ_Kullanicilar_Mail
-UNIQUE (mail);
